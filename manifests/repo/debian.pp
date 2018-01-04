0ba25b4eef51be3f447db3ca7e2ca10b356b2c11
Rick Steckles <r.steckles@ctidigital.com>
2018-01-04 09:37:33 +0000

    Attempt to fix invalid parameter being passed to ::packagecloud::repo

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -4,6 +4,8 @@ class varnish::repo::debian {
 
     $ver = delete($::varnish::varnish_version,'.')
 
+    # TODO: Determine how the $dist parameter is used (there is no
+    # 'distribution' parameter to '::packagecloud::repo')
     if $ver <= "30" and $::lsbdistcodename == "xenial" {
       $dist = "trusty"
     } else {
@@ -13,7 +15,6 @@ class varnish::repo::debian {
     ::packagecloud::repo { 'varnish-cache':
       fq_name => "varnishcache/varnish${ver}",
       type    => 'deb',
-      distribution => $dist,
     }
 
     exec { 'varnish-cache apt-update':
